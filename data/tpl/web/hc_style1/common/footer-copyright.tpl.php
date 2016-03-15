0262ea85003970421a88e40ab90e61902aa436fc
augustanshu <2430972123@qq.com>
2016-03-15 12:12:18 +0800

    Signed-off-by: augustanshu <2430972123@qq.com> ~

--- data/tpl/web/hc_style1/common/footer-copyright.tpl.php
+++ data/tpl/web/hc_style1/common/footer-copyright.tpl.php
@@ -4,7 +4,7 @@
 	<div class="center-block footer" role="footer">
 		<div class="text-center">
 			<?php  if(empty($_W['setting']['copyright']['footerright'])) { ?>
-			Powered by 宁波港通天下网络科技有限公司</a> © 2015-2016 
+			Powered by 宁波港通天下网络科技有限公司.</a> © 2015-2016 
 			<?php  } else { ?><?php  echo $_W['setting']['copyright']['footerright'];?><?php  } ?> &nbsp; &nbsp; <?php  if(!empty($_W['setting']['copyright']['statcode'])) { ?><?php  echo $_W['setting']['copyright']['statcode'];?><?php  } ?>
 		</div>
 	</div>
