cc3f0c71ebd9058ccbc2469871faf40a86c9e27d
augustanshu <2430972123@qq.com>
2016-03-15 11:06:17 +0800

    Signed-off-by: augustanshu <2430972123@qq.com> initial

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright © 2015-2016 Port-of-World.
+ *  2016-03-03 
+ */
+include'./framework/bootstrap.inc.php';
+$host = $_SERVER['SERVER_NAME'];
+if (!empty($host)) {
+	$bindhost = pdo_fetch("SELECT * FROM ".tablename('site_multi')." WHERE bindhost = :bindhost", array(':bindhost' => $host));
+	if (!empty($bindhost)) {
+		header("Location: ". $_W['siteroot'] . 'app/index.php?i='.$bindhost['uniacid'].'&t='.$bindhost['id']);
+		exit;
+	}
+}
+if($_W['os'] == 'mobile' && (!empty($_GPC['i']) || !empty($_SERVER['QUERY_STRING']))) {
+	header('Location: ./app/index.php?' . $_SERVER['QUERY_STRING']);
+} else {
+	header('Location: ./web/index.php?' . $_SERVER['QUERY_STRING']);
+}
